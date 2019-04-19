3fdef58c7edbde4d43ce5433f79f279fe3b731aa
luogang <luogang@yuuxin.com>
2019-04-19 10:51:07 +0800

    将业务线程提交至线程池

--- src/main/java/net/iwinter/core/handle/HttpServerDispatcherHandler.java
+++ src/main/java/net/iwinter/core/handle/HttpServerDispatcherHandler.java
@@ -8,12 +8,18 @@ import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.util.CharsetUtil;
 import net.iwinter.core.bean.HandleRequestURL;
 import net.iwinter.core.route.RouteProcess;
+import net.iwinter.core.thread.HandleRequestThread;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.URLDecoder;
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: luogang
@@ -23,23 +29,34 @@ import java.util.Map;
 @ChannelHandler.Sharable
 public class HttpServerDispatcherHandler extends SimpleChannelInboundHandler<DefaultHttpRequest> {
     private static final Logger log = LoggerFactory.getLogger(HttpServerDispatcherHandler.class);
+
     private static final Map<String, HandleRequestURL> requestUrlMap = HandlerRequestMapping.requestUrlMap;
+    private static final ThreadPoolExecutor threadPoolExecutor =
+                                            new ThreadPoolExecutor(1, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue(),new HandleRequestThread());
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, DefaultHttpRequest defaultHttpRequest) throws Exception {
-        log.info("request url:{}", defaultHttpRequest.getUri());
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, DefaultHttpRequest defaultHttpRequest){
+        threadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    log.info("request url:{}", defaultHttpRequest.getUri());
+                    QueryStringDecoder queryStringDecoder = new QueryStringDecoder(URLDecoder.decode(defaultHttpRequest.getUri()), CharsetUtil.UTF_8);
 
-        QueryStringDecoder queryStringDecoder = new QueryStringDecoder(URLDecoder.decode(defaultHttpRequest.getUri()), CharsetUtil.UTF_8);
+                    String requestUrl = RouteProcess.validateRequestUrl(queryStringDecoder);
+                    if (StringUtils.isEmpty(requestUrl)) {
+                        channelHandlerContext.close();
+                        throw new NullPointerException(defaultHttpRequest.getUri());
+                    }
 
-        String requestUrl = RouteProcess.validateRequestUrl(queryStringDecoder);
-        if (StringUtils.isEmpty(requestUrl)) {
-            channelHandlerContext.close();
-            throw new NullPointerException(defaultHttpRequest.getUri());
-        }
-
-        HandleRequestURL requestURL = requestUrlMap.get(requestUrl);
-        Object[] objects = RouteProcess.parseRouteParameter(requestURL, queryStringDecoder);
-        RouteProcess.excute(channelHandlerContext, requestURL, objects);
+                    HandleRequestURL requestURL = requestUrlMap.get(requestUrl);
+                    Object[] objects = RouteProcess.parseRouteParameter(requestURL, queryStringDecoder);
+                    RouteProcess.excute(channelHandlerContext, requestURL, objects);
+                } catch (Exception e) {
+                    log.warn("handle request erro:{}",e);
+                }
+            }
+        });
     }
 
 }
